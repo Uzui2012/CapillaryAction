ebfb61ceca3abcbb6a7f94c018622f5458dbc758
Killian <killian2100@hotmail.com>
2025-04-09 17:01:53 +0100

    Colour Edit

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const static = 1;
 const empty = 0;
 let grid;
 let nextGrid;
-const squareWidth = 10;
+const squareWidth = 15;
 let cols, rows;
 
 // Makes 2D Array of size cols by rows
@@ -36,7 +36,7 @@ function getNextGrid() {
             let state = grid[i][j];
 
             if(state == 1){ // Liquid is currently not in freefall
-                // Check if liquid is now in freefall
+
                 if(j+1 > rows){
                     nextGrid[i][j] = 1;
                 }else{
@@ -49,7 +49,7 @@ function getNextGrid() {
                         let stateAR = grid[i+1][j-1];
                         let below = grid[i][j+1];
 
-                        if(stateBR > 0 && stateR === 0 && stateAR === 0 
+                        if(stateBR !== 0 && stateR === 0 && stateAR === 0 
                             && below === static && nextGrid[i+1][j] === 0){
                             nextGrid[i+1][j] = static;
                         }else if(grid[i][j+1] === 0){
@@ -179,24 +179,30 @@ function mouseDragged() {
 // Both setup() and draw() are called on saving of this file.
 // createCanvas creates and renders a canvas space on index.html
 function setup() {
-    createCanvas(500, 500);
-    frameRate(6000);
+    createCanvas(300,300);
+    frameRate(60000);
     initGrid();
 }
 
 // Rendering occurs in here, 60 times a second (default)
 function draw() {
-    background(220);
+    background(255);
+    let water = color(50, 100, 255); // RGB
+    let waterFall = color(80, 150, 255); // RGB
+    let tank = color(50, 50, 50);
     for (let i = 0; i < cols; i++){
         for (let j = 0; j < rows; j++){
-            stroke(255);
-            if(grid[i][j] == 1){
-                fill(255);
-            }else if(grid[i][j] == 2){
-                 fill(100);
-            }else if(grid[i][j] == 0){
-                 fill(0);
+            if(grid[i][j] == static){
+                stroke(water);
+                fill(water);
+            }else if(grid[i][j] == falling){
+                stroke(waterFall);
+                fill(waterFall);
+            }else if(grid[i][j] == empty){
+                stroke(tank);
+                 fill(tank);
             }else{
+                stroke(255);
                 fill(180);
             }
             let x = i * squareWidth;
